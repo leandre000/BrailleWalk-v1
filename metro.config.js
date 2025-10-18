39278a941a989c90ee7f6fc0fcd4124fbd11ad64
Shema Leandre <iamshemaleandre@gmail.com>
2025-10-18 06:34:14 +0200

    fixed TurboModule error

--- metro.config.js
+++ metro.config.js
@@ -2,21 +2,5 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-// Add support for additional file extensions
-config.resolver.assetExts.push(
-  'db',
-  'mp3',
-  'ttf',
-  'obj',
-  'png',
-  'jpg'
-);
-
-// Configure platform extensions for web
-config.resolver.platforms = ['web', 'ios', 'android'];
-
-// Add source extensions
-config.resolver.sourceExts.push('jsx', 'js', 'ts', 'tsx', 'json', 'cjs', 'mjs');
-
 module.exports = config;
 
